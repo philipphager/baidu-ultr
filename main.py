bce91ac86fb84b05ea977bb3a824fea348e50b73
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-10-09 17:34:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from baidu_ultr.util import download_model, DatasetWriter
 
 @hydra.main(version_base="1.3", config_path="config", config_name="config")
 def main(config):
-    device = torch.device("mps")
+    device = torch.device("cuda:0")
     model_path = Path(config.model_directory) / config.model
 
     in_path = Path(config.in_directory) / config.in_file
