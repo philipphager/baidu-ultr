6d6ce9007c38f5732db049138bfda964f3bc50aa
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-10-09 09:17:42 +0200

    Add hydra

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from pathlib import Path
 
+import hydra
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import BertModel
 
-from baidu_ultr.data import BaiduTestDataset
+from baidu_ultr.data import BaiduTestDataset, BaiduTrainDataset
 from baidu_ultr.util import download_model
 
-if __name__ == "__main__":
-    in_file = Path("data") / "annotation_data_0522.txt"
-    out_file = Path("output") / "test.svm"
 
-    model = "base_group2"
-    model_directory = Path("models")
-
+@hydra.main(version_base="1.3", config_path="config", config_name="config")
+def main(config):
     device = torch.device("mps")
-    batch_size = 8
-    max_sequence_length = 128
-    model_path = model_directory / model
+    model_path = Path(config.model_directory) / config.model
 
     if not model_path.exists():
-        download_model(model_directory, model)
+        download_model(config.model_directory, config.model)
 
-    train_dataset = BaiduTestDataset(in_file, max_sequence_length)
+    if config.feedback == "click":
+        dataset = BaiduTrainDataset(config.in_path, config.max_sequence_length)
+    elif config.feedback == "rating":
+        dataset = BaiduTestDataset(config.in_path, config.max_sequence_length)
+    else:
+        raise ValueError("config.file_type must be train or validation")
 
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        num_workers=1,
+    dataset_loader = DataLoader(
+        dataset,
+        batch_size=config.batch_size,
         pin_memory=True,
     )
 
@@ -36,9 +35,9 @@ if __name__ == "__main__":
     model.to(device)
     torch.compile(model)
 
-    out_file.unlink(missing_ok=True)
+    Path(config.out_path).unlink(missing_ok=True)
 
-    for i, batch in tqdm(enumerate(train_loader)):
+    for i, batch in tqdm(enumerate(dataset_loader)):
         query_ids, clicks, tokens, attention_mask, token_types = batch
 
         model_output = model(
@@ -47,3 +46,7 @@ if __name__ == "__main__":
             token_types.to(device),
         )
         features = model_output.pooler_output
+
+
+if __name__ == "__main__":
+    main()
