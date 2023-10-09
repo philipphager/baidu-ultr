5309a62d6c67edbfebfbe7d83c8b8a18da1ee4de
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-10-09 19:08:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task 16
-#SBATCH --mem 120 GB
+#SBATCH --mem 120GB
 #SBATCH --partition gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END
