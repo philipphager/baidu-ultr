30e891c204fda3c82920571a0e648cab0baed8c0
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-23 11:24:10 +0100

    Update tencent masking

--- baidu_ultr/model/tencent.py
+++ baidu_ultr/model/tencent.py
@@ -1,24 +1,38 @@
+from pathlib import Path
+from typing import Dict
+
 import torch
 from torch import nn
 from transformers import BertModel
 
-from baidu_ultr.const import TOKEN_OFFSET
+from baidu_ultr.const import TOKEN_OFFSET, TENCENT_SPECIAL_TOKENS
 
 
 class TencentModel(nn.Module):
-    def __init__(self, path, device):
+    def __init__(self, model_directory: str, model_name: str):
         super().__init__()
-        self.path = path
-        self.device = device
+        self.model_directory = Path(model_directory)
+        self.model_name = model_name
+        self.model = None
+        self.device = None
 
-    def load(self):
-        model = BertModel.from_pretrained(self.path, local_files_only=True)
-        model.to(self.device)
+    def load(self, device):
+        path = self.model_directory / self.model_name
+        assert path.exists(), f"""
+        Please download the Baidu 12Layer pre-trained language model from Google Drive:
+        https://drive.google.com/file/d/1KWOd2TsFwgWIAMDBv9mVXQ4D5h6s5tWy/view
+        
+        and extract it in the model directory: {self.model_directory}
+        """
+        model = BertModel.from_pretrained(path, local_files_only=True)
+        model.to(device)
         torch.compile(model)
         self.model = model
+        self.device = device
 
     def forward(self, tokens, token_types):
-        mask = self.mask_attention(tokens, TOKEN_OFFSET)
+        mask = self.mask_attention(tokens, TENCENT_SPECIAL_TOKENS)
+        print(mask[0], tokens[0])
         output = self.model(
             input_ids=tokens.to(self.device),
             attention_mask=mask.to(self.device),
@@ -28,8 +42,8 @@ class TencentModel(nn.Module):
         return output.pooler_output
 
     @staticmethod
-    def mask_attention(tokens: torch.IntTensor, offset: int):
+    def mask_attention(tokens: torch.IntTensor, special_token: Dict[str, int]):
         """
         Hugging face attention mask, is True for all tokens that should NOT be masked.
         """
-        return tokens >= offset
+        return tokens > special_token["PAD"]
