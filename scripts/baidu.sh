298b15640be4dfc99e09a6067e91d0c8cdf7aa40
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-11-24 14:07:07 +0100

    Fix typo

--- scripts/baidu.sh
+++ scripts/baidu.sh
@@ -22,10 +22,10 @@ python main.py \
   train_split_id=$SLURM_ARRAY_TASK_ID \
   model="$MODEL" \
   tokens="$MODEL" \
-  output_directory=data/"$MODEL"
+  out_directory=data/"$MODEL"
 
 python main.py \
   data_type=val \
   model="$MODEL" \
   tokens="$MODEL" \
-  output_directory=data/"$MODEL"
+  out_directory=data/"$MODEL"
