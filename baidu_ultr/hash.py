8509164ba588bd012ebe21dbe33d888fca3e07e3
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-12-19 13:37:25 +0100

    Update hash.py

--- baidu_ultr/hash.py
+++ baidu_ultr/hash.py
@@ -5,5 +5,4 @@ def md5(text: str) -> str:
     tokens = text.split(b"\x01")
     tokens = " ".join([t.decode() for t in tokens]).encode('utf-8')
     hashed_tokens = hashlib.md5(tokens)
-
     return hashed_tokens.hexdigest()
