1958301e693c02e4067bf524fa3e1b4830283805
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-11 18:02:30 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import hydra
-from omegaconf import OmegaConf
 
 from baidu_ultr.const import MISSING_TITLE, WHAT_OTHER_PEOPLE_SEARCHED_TITLE
 from baidu_ultr.model.ltr.indexer import Indexer
@@ -9,8 +8,6 @@ from baidu_ultr.model.ltr.indexer import Indexer
 
 @hydra.main(version_base="1.3", config_path="config", config_name="config")
 def main(config):
-    print(OmegaConf.to_yaml(config))
-
     directory = Path(config.data_directory)
     train_files = [f for f in directory.glob("part-*")]
     output_path = Path(config.out_directory) / "index.json"
