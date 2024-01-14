4eb25b487f135eb387dfb7a8b760a0e0d60fb2bb
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-14 17:27:38 +0100

    Update scripts

--- scripts/val.sh
+++ scripts/val.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=uva
+#SBATCH --job-name=val
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task 8
@@ -9,23 +9,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=p.k.hager@uva.nl
-#SBATCH --array=0-9
 
-MODEL=uva
-
+echo "Model: $1"
 source ${HOME}/.bashrc
 mamba activate baidu-ultr-features
 
 python main.py \
-  train_part=0 \
-  data_type=train \
-  train_split_id=$SLURM_ARRAY_TASK_ID \
-  model="$MODEL" \
-  tokens="$MODEL" \
-  out_directory=output/"$MODEL"
-
-python main.py \
   data_type=val \
-  model="$MODEL" \
-  tokens="$MODEL" \
-  out_directory=output/"$MODEL"
+  model="$1" \
+  tokens="$1" \
+  out_directory=output/"$1"
