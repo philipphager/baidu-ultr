843880dfffe9621e8ab52bb3c1807e7aad0828dd
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-15 11:25:35 +0100

    Add configs to load retro-mae checkpoints and refactor UvA loader

--- src/model/uva.py
+++ src/model/uva.py
@@ -2,10 +2,9 @@ from pathlib import Path
 from typing import Dict, Optional
 
 import torch
-from torch import LongTensor, IntTensor, BoolTensor, FloatTensor
+from torch import LongTensor, IntTensor, BoolTensor
 from torch import nn
-from torch.nn import CrossEntropyLoss
-from transformers import BertModel, PretrainedConfig, AutoConfig, BertForPreTraining
+from transformers import PretrainedConfig, AutoConfig, BertForPreTraining
 from transformers.models.bert.modeling_bert import BertLMPredictionHead
 
 
@@ -22,11 +21,10 @@ class UvaModel(nn.Module):
         path = self.model_directory / self.model_name
         assert path.exists(), f"""
         Please download the Baidu 12Layer pre-trained language model from:
-        # FIXME!
         and extract it in the model directory: {self.model_directory}
         """
         config = AutoConfig.from_pretrained(path)
-        model = CrossEncoder.from_pretrained(path, local_files_only=True, config=config)
+        model = BertModel.from_pretrained(path, local_files_only=True, config=config)
         model.to(device)
         torch.compile(model)
         self.model = model
@@ -39,7 +37,7 @@ class UvaModel(nn.Module):
         **kwargs,
     ):
         mask = self.mask_attention(tokens, self.special_tokens)
-        _, query_document_embedding = self.model(
+        query_document_embedding = self.model(
             tokens=tokens.to(self.device),
             attention_mask=mask.to(self.device),
             token_types=token_types.to(self.device),
@@ -55,15 +53,9 @@ class UvaModel(nn.Module):
 
 
 class BertModel(BertForPreTraining):
-    """
-    Basic BERT model pre-trained only on the MLM task (i.e. RoBERTa setup).
-    The model can be further fine-tuned in a CrossEncoder or Condenser setup.
-    """
-
     def __init__(self, config: PretrainedConfig):
         super(BertModel, self).__init__(config)
         self.mlm_head = BertLMPredictionHead(config)
-        self.mlm_loss = CrossEntropyLoss()
 
     def forward(
         self,
@@ -73,65 +65,13 @@ class BertModel(BertForPreTraining):
         labels: Optional[LongTensor] = None,
         **kwargs,
     ):
-        loss = 0
         outputs = self.bert(
             input_ids=tokens,
             attention_mask=attention_mask,
             token_type_ids=token_types,
+            output_hidden_states=True,
             return_dict=True,
         )
-        query_document_embedding = outputs.pooler_output
 
-        if labels is not None:
-            loss += self.get_mlm_loss(outputs[0], labels)
-
-        return loss, query_document_embedding
-
-    def get_mlm_loss(self, sequence_output: FloatTensor, labels: LongTensor):
-        token_scores = self.mlm_head(sequence_output)
-
-        return self.mlm_loss(
-            token_scores.view(-1, self.config.vocab_size),
-            labels.view(-1),
-        )
-
-
-class CrossEncoder(BertModel):
-    """
-    BERT cross-encoder: https://arxiv.org/abs/1910.14424
-    Query and document are concatenated in the input. The prediction targets are an MLM
-    task and a relevance prediction task using the CLS token. To reproduce the original
-    model released by Baidu, we use clicks or annotations as the relevance signal.
-    """
-
-    def __init__(self, config: PretrainedConfig):
-        super(CrossEncoder, self).__init__(config)
-        self.click_head = nn.Linear(config.hidden_size, 1)
-        self.click_loss = nn.BCEWithLogitsLoss()
-
-    def forward(
-        self,
-        tokens: LongTensor,
-        attention_mask: BoolTensor,
-        token_types: IntTensor,
-        labels: Optional[LongTensor] = None,
-        clicks: Optional[FloatTensor] = None,
-        **kwargs,
-    ):
-        loss = 0
-        outputs = self.bert(
-            input_ids=tokens,
-            attention_mask=attention_mask,
-            token_type_ids=token_types,
-            return_dict=True,
-        )
-        query_document_embedding = outputs.pooler_output
-
-        if clicks is not None:
-            click_scores = self.click_head(query_document_embedding).squeeze()
-            loss += self.click_loss(click_scores, clicks)
-
-        if labels is not None:
-            loss += self.get_mlm_loss(outputs[0], labels)
-
-        return loss, query_document_embedding
+        cls = outputs.hidden_states[-1][:, 0]
+        return cls
