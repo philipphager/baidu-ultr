0956ee54046c4fe39cfe1fff3883e78116311965
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-18 10:20:35 +0100

    Change dataset writer

--- src/util.py
+++ src/util.py
@@ -1,20 +1,23 @@
 from collections import defaultdict
 from pathlib import Path
+from typing import List, Any
 from zipfile import ZipFile
 
 import pandas as pd
 import torch
 import wget
+from pyarrow import Tensor
+
+from src.const import TOKEN_OFFSET
+from src.data import split_idx
 
 
 class DatasetWriter:
     def __init__(
         self,
         half_precision: bool,
-        min_docs_per_query: int,
     ):
         self.half_precision = half_precision
-        self.min_docs_per_query = min_docs_per_query
         self.features = defaultdict(lambda: [])
         self.embeddings = []
 
@@ -29,30 +32,30 @@ class DatasetWriter:
 
     def save(self, path: Path):
         for k, v in self.features.items():
-            if "md5" in k or k == "query_id":
-                self.features[k] = [h for hashes in v for h in hashes]
-            else:
+            if k in [
+                "position",
+                "media_type",
+                "displayed_time",
+                "serp_height",
+                "slipoff_count_after_click",
+                "click",
+            ]:
                 self.features[k] = list(torch.concat(self.features[k]).numpy())
+            else:
+                if k in ["query", "title", "abstract"]:
+                    self.features[k] = [split_idx(t, TOKEN_OFFSET) for t in flatten(v)]
+                else:
+                    self.features[k] = flatten(v)
 
         self.features["query_document_embedding"] = list(
             torch.vstack(self.embeddings).numpy()
         )
         df = pd.DataFrame(self.features)
-        df = self.filter_queries(df)
         df.to_feather(path)
 
-    def filter_queries(self, df):
-        query_df = df.groupby("query_id").agg(n_docs=("text_md5", "count")).reset_index()
 
-        n_queries_before = len(query_df)
-        query_df = query_df[query_df.n_docs >= self.min_docs_per_query]
-        n_queries_after = len(query_df)
-        print(
-            f"Dropped {n_queries_before - n_queries_after} queries",
-            f"with less than {self.min_docs_per_query} docs",
-        )
-
-        return df[df.query_id.isin(set(query_df.query_id))]
+def flatten(lists: List[List[Any]]) -> List[Any]:
+    return [item for _list in lists for item in _list]
 
 
 def download_model(directory: Path, name: str):
