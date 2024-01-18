5ca6e1b73582dfe87d2167c20e3c82c4882d5fe3
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-18 10:20:17 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,18 +2,18 @@ from pathlib import Path
 
 import hydra
 
-from src.const import TITLES
-from src.index import Indexer, save_index
+from src.const import Title
+from src.lexical.indexer import Indexer, save_index
 
 
 @hydra.main(version_base="1.3", config_path="config", config_name="config")
 def main(config):
     directory = Path(config.data_directory)
-    train_files = [f for f in sorted(directory.glob("part-*"))][:config.index_parts]
+    train_files = [f for f in sorted(directory.glob("part-*"))][: config.index_parts]
     output_path = Path(config.out_directory) / "index.json"
 
     # Optionally skip documents based on their title
-    ignored_titles = [TITLES[t] for t in config.ignored_titles]
+    ignored_titles = [Title.MISSING.value, Title.WHAT_OTHERS_SEARCHED.value]
 
     indexer = Indexer(
         train_files,
